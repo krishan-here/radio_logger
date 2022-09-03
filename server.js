ad232628586cf8f4f71eb83c93fcd08451d03d0c
krishan-here <iamkrishan111@gmail.com>
2022-09-03 15:33:03 +0530

    express routes fixed and dashboard redirect correctly

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser'); // Middleware
+const componentsDirUrl = './src/client/src/components';
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -14,29 +15,28 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.get('/', (req, res) => {
-  res.sendFile('./index.html', { root: __dirname });
+app.get('/dashboard', (req, res) => {
+  res.sendFile(componentsDirUrl + '/dashboard.html', { root: __dirname });
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile('./about.html', { root: __dirname });
+app.get('/contestlog', (req, res) => {
+  res.sendFile(componentsDirUrl + '/contestlog.html', { root: __dirname });
 });
 
-app.get('/contact', (req, res) => {
-  res.sendFile('./contact.html', { root: __dirname });
+app.get('/swllog', (req, res) => {
+  res.sendFile(componentsDirUrl + '/swllog.html', { root: __dirname });
 });
 
-router.get('/', function(req, res) {
-  res.send('Wiki home page');
+app.get('/signin', function(req, res) {
+  res.sendFile(componentsDirUrl + '/signin.html', { root: __dirname });
 });
 
-// About page route
-router.get('/about', function(req, res) {
-  res.send('About this wiki');
+app.get('/hamlog', function(req, res) {
+  res.sendFile(componentsDirUrl + '/hamlog.html', { root: __dirname });
 });
 
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/src/client/src/components/signin.html');
+app.get('/signup', function(req, res) {
+  res.sendFile(componentsDirUrl + '/signup.html', { root: __dirname });
 });
 
 app.post('/login', (req, res) => {
